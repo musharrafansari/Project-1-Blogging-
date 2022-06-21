a69b9078644e94b564a7afc97f251747b1d7ebe2
Suman Bera <suman80bera@gmail.com>
2022-06-21 16:19:11 +0530

    update

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -1,17 +1,18 @@
 const authModel = require('../model/authorModel')
 
-const createAuthor = async function(req,res) {
+const createAuthor =  async function(req,res) {
 try{
 let data = req.body
 
-if(!Object.keys(data).length  ){
+if(!Object.keys(data).length){
   res.status(400).send({ status: false, msg: "Author data required"})
 }    
 
-
+else{
 let savedata = await authModel.create(data)
+res.status(201).send({status: true,msg: savedata})
+}
 
-res.status(201).send({status: true, msg: savedata})
 }catch(err){
     res.status(500).send({status: false, msg: 'server not respond'})
 }
